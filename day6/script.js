d7f298da6bec6f7310bf95ff6b6a216a9cd8ca75
Hariom dangi <hdangi224@gmail.com>
2025-05-13 18:25:46 +0530

    Array

---
+++ day6/script.js
@@ -0,0 +1,46 @@
+// Arrays
+
+// let myArr = [1, 2, 3, 4, 5, 6, 7, "Akshay"];
+
+// console.log(myArr);
+
+// for of
+// forEach
+// map
+
+// for (let elem of myArr) {
+//   console.log(elem);
+// }
+
+let myArr = [1, 2, 3, 4, 5, 6, 7, "Akshay"];
+
+// myArr.push(9)
+
+// myArr.pop()
+
+// myArr.unshift(66)
+
+// myArr.shift()
+
+// console.log(myArr);
+
+// let newArr = myArr.slice(1, 7);
+// console.log(newArr);
+// console.log(myArr);
+
+// let dele = myArr.splice(1, 2);
+// myArr.splice(4, 0, "grapes");
+
+// console.log(myArr);
+// console.log(dele);
+
+
+// let newArr = [...myArr]
+// newArr.pop()
+
+// console.log(newArr);
+// console.log(myArr);
+
+
+
+
