8ee502e0004923a20cd4beb6539bd9d937278950
Hariom dangi <hdangi224@gmail.com>
2025-05-14 18:25:20 +0530

    functions

---
+++ day7/script.js
@@ -0,0 +1,29 @@
+// ====================== FUNCTIONS =======================
+
+// function greet(name, age) {
+//   console.log(`Hello ${name} Your age is ${age}`);
+// }
+
+// greet("Akshay", 23)
+// greet("Naman", 45)
+
+// let greet = (name) => {
+//     console.log(name);
+
+// };
+
+// greet("Akshay")
+// greet("Aksdsfdsfsdfdfhay")
+
+function num(n) {
+  console.log("Hello ji hello");
+
+  return n + 5;
+
+  console.log("Helllo");
+  
+}
+
+let value = num(20);
+
+console.log(value);
